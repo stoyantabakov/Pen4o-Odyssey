4d2a680a6ce63c8039366419d6a662d0dd96ae28
Stoyan Tabakov <st.tabakov@gmail.com>
2025-01-14 13:28:09 +0200

    Adding small corrections

--- week-2.js
+++ week-2.js
@@ -1,25 +1,22 @@
-function inputDoorNumber(doorNumber) {
+function guessTreasureDoor(doorNumber) {
     switch (doorNumber) {
         case 1:
         case 2:
         case 3:
             let goldDoorNumber = Math.floor(Math.random() * 3) + 1; // Formula for random number in diapason -> Math.floor(Math.random() * (max - min + 1)) + min;
-            
+
             if (doorNumber === goldDoorNumber) {
                 console.log(`Congratulations, Pen4o! You found the treasure behind Door ${doorNumber}!`);
-
             } else {
-                console.log('A bucket of cold water splashes on you! Unlucky Pen4o!');
-
+                console.log(`A bucket of cold water splashes on you! Unlucky Pen4o!`);
             }
 
-        break;
+            break;
 
-        default: 
-            console.log('Invalid choice. Please select Door 1, 2, or 3.'); break;
-
+        default:
+            console.log("Invalid choice. Please select Door 1, 2, or 3.");
+            break;
     }
-
 }
 
-inputDoorNumber(3);
+guessTreasureDoor(3);
