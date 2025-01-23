9714a227cda55431407e08636110a97b6627cb80
Stoyan Tabakov <st.tabakov@gmail.com>
2025-01-23 18:08:00 +0200

    Adding small corrections

--- week-3.js
+++ week-3.js
@@ -1,7 +1,7 @@
 function decodingChipher(chipher) {
 
     let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    let smallCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     let decodedMessage = '';
     let cardPower = 0;
@@ -9,7 +9,7 @@ function decodingChipher(chipher) {
     for (let i = 0; i < chipher.length; i++) {
         let chipherChar = chipher[i];
 
-        if (smallCase.includes(chipherChar)) {
+        if (lowerCase.includes(chipherChar)) {
             decodedMessage += chipherChar.toUpperCase();
         } else if (upperCase.includes(chipherChar)) {
             decodedMessage += chipherChar.toLowerCase();
