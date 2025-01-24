b8cf5cb2bd32d88b4be127b0de5803845f58977b
Stoyan Tabakov <st.tabakov@gmail.com>
2025-01-24 08:20:31 +0200

    Upload Week 3 challenge

---
+++ week-3-regex.js
@@ -0,0 +1,32 @@
+function decodingChipher(chipher) {
+    let upperCasePattern = /[A-Z]/;
+    let lowerCasePattern = /[a-z]/;
+    let digitPattern = /\d/;
+    let decodedMessage = '';
+    let cardPower = 0;
+
+    for (let i = 0; i < chipher.length; i++) {
+        let chipherChar = chipher[i];
+
+        if (lowerCasePattern.test(chipherChar)) {
+            decodedMessage += chipherChar.toUpperCase();
+        } else if (upperCasePattern.test(chipherChar)) {
+            decodedMessage += chipherChar.toLowerCase();
+        } else if (digitPattern.test(chipherChar)) {
+            cardPower += Number(chipherChar);
+            decodedMessage += chipherChar;
+        } else {
+            decodedMessage += chipherChar;
+        }
+    }
+
+    console.log(decodedMessage);
+
+    if (cardPower > 20) {
+        console.log(`The map reveals the your next location! Total power: ${cardPower}`);
+    } else {
+        console.log(`The map warns of danger! Total power: ${cardPower}`);
+    }
+}
+
+decodingChipher('T3r4p_M4p');
